bf116d2148a0646e43837e48de5c58bfe68741e0
origin <mustafa.hegy94@gmail.com>
2021-12-24 14:35:30 +0200

    Initial Commit

---
+++ Show_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Show_Task
+//
+//  Created by Mostafa on 24/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
